3d346c26ef44ca30c4948aa011c93f194a4a5553
Karol Grzegorczyk <kgr@agh.edu.pl>
2014-12-15 09:22:08 +0100

    Graph corrections

--- graph.py
+++ graph.py
@@ -10,7 +10,7 @@ class Graph:
     def adjacencies(self, vertex):
         return self.__adjacencies[vertex]
 
-    def get_edge_number(self):
+    def get_vertex_number(self):
         return len(self.__adjacencies)
 
 
@@ -24,7 +24,7 @@ def has_path_to(graph, start_vertex, target_vertex):
             if not explored[adj_vertex] and nested(adj_vertex, explored):
                 return True
         return False
-    return nested(start_vertex, [False] * graph.get_edge_number())
+    return nested(start_vertex, [False] * graph.get_vertex_number())
 
 
 def get_shortest_path_to(graph, start_vertex, target_vertex):
@@ -38,9 +38,9 @@ def get_shortest_path_to(graph, start_vertex, target_vertex):
     from collections import deque
     queue = deque()
     queue.appendleft(start_vertex)
-    explored = [False] * graph.get_edge_number()
+    explored = [False] * graph.get_vertex_number()
     explored[start_vertex] = True
-    edge_to = [None] * graph.get_edge_number()
+    edge_to = [None] * graph.get_vertex_number()
     while len(queue) != 0:
         current_vertex = queue.pop()
         for adj_vertex in graph.adjacencies(current_vertex):
@@ -61,6 +61,8 @@ if __name__ == '__main__':
     """
     g = Graph(7)
 
+    assert g.get_vertex_number() == 7
+
     g.add_edge(0, 1)
     g.add_edge(1, 2)
     g.add_edge(3, 4)
@@ -70,13 +72,18 @@ if __name__ == '__main__':
     g.add_edge(1, 5)
     g.add_edge(2, 5)
 
+    assert set(g.adjacencies(5)) == set([1, 2, 4])
+    assert g.adjacencies(6) == []
+
     assert has_path_to(g, 3, 2)
     assert has_path_to(g, 0, 5)
     assert not has_path_to(g, 3, 6)
 
-    assert get_shortest_path_to(g, 0, 5) == [0, 1, 5]
     assert get_shortest_path_to(g, 3, 1) == [3, 0, 1]
-    assert get_shortest_path_to(g, 3, 2) == [3, 4, 5, 2]
+    sp05 = get_shortest_path_to(g, 0, 5)
+    assert sp05 == [0, 1, 5] or sp05 == [0, 4, 5]
+    sp32 = get_shortest_path_to(g, 3, 2)
+    assert sp32 == [3, 0, 1, 2] or sp32 == [3, 4, 5, 2]
     assert not get_shortest_path_to(g, 0, 6)
 
     print('All tests passed')
