0e265a1ba751330ba30f2cc3d872dd287d9369ea
Karol Grzegorczyk <kgr@agh.edu.pl>
2014-12-29 15:07:13 +0100

    Dijkstra's algorithm for shortest path; Prim's algorithm for minimum spanning tree

--- graph.py
+++ graph.py
@@ -1,14 +1,15 @@
 class Graph:
     """Adjacency-list representation of a graph"""
     def __init__(self, vertex_number):
-        self.__adjacencies = [[] for i in range(vertex_number)]
+        self.__adjacencies = [{} for i in range(vertex_number)]
 
-    def add_edge(self, a, b):
-        self.__adjacencies[a].append(b)
-        self.__adjacencies[b].append(a)
+    def add_edge(self, a, b, length=1, undirected=True):
+        self.__adjacencies[a][b] = length
+        if undirected:
+            self.__adjacencies[b][a] = length
 
-    def adjacencies(self, vertex):
-        return self.__adjacencies[vertex]
+    def adjacencies(self, vertex, with_length=False):
+        return self.__adjacencies[vertex] if with_length else self.__adjacencies[vertex].keys()
 
     def get_vertex_number(self):
         return len(self.__adjacencies)
@@ -27,14 +28,16 @@ def has_path_to(graph, start_vertex, target_vertex):
     return nested(start_vertex, [False] * graph.get_vertex_number())
 
 
-def get_shortest_path_to(graph, start_vertex, target_vertex):
-    """Breadth-First Search (BFS)"""
-    def get_path(vertex):
+def get_path(vertex, previous):
         if vertex is None:
             return []
-        path = get_path(edge_to[vertex])
+        path = get_path(previous[vertex], previous)
         path.append(vertex)
         return path
+
+
+def get_shortest_path_to(graph, start_vertex, target_vertex):
+    """Breadth-First Search (BFS)"""
     from collections import deque
     queue = deque()
     queue.appendleft(start_vertex)
@@ -47,16 +50,74 @@ def get_shortest_path_to(graph, start_vertex, target_vertex):
             if not explored[adj_vertex]:
                 edge_to[adj_vertex] = current_vertex
                 if adj_vertex == target_vertex:
-                    return get_path(target_vertex)
+                    return get_path(target_vertex, edge_to)
                 explored[adj_vertex] = True
                 queue.appendleft(adj_vertex)
 
 
+def build_mst(graph):
+    """Prim's MST algorithm; eager implementation"""
+    from heap import Heap
+
+    start_vertex = 0  # does not matter where we start building MST
+    to_explore = Heap()  # values are vertex ids; keys are distances from the tree to the vertices
+    to_explore.push(0, start_vertex)
+    mst = []
+    mst_edges = []
+    previous = [None] * graph.get_vertex_number()  # previous vertex in the tree
+    while to_explore.size() > 0:
+        _, current_vertex = to_explore.pop()
+        mst_edges.append(current_vertex)
+        if previous[current_vertex] is not None:
+            mst.append((previous[current_vertex], current_vertex) if previous[current_vertex] < current_vertex else (current_vertex, previous[current_vertex]))
+        for adj_vertex, edge_length in graph.adjacencies(current_vertex, True).items():
+            if adj_vertex in mst_edges:
+                continue
+            adj_length = to_explore.get_key(adj_vertex)
+            if not adj_length:
+                to_explore.push(edge_length, adj_vertex)
+                previous[adj_vertex] = current_vertex
+            elif edge_length < adj_length:
+                to_explore.decrease_key(edge_length, adj_vertex)
+                previous[adj_vertex] = current_vertex
+    return set(mst)
+
+
+def get_dijkstra_shortest_path_to(graph, start_vertex, target_vertex):
+    """Dijkstra's shortest-path algorithm
+    All edges are assumed to be non-negative
+    The graph is assumed to be directed"""
+    import sys
+    from heap import Heap
+
+    to_explore = Heap()
+    to_explore.push(0, start_vertex)
+    dist_to = [sys.maxsize] * graph.get_vertex_number()
+    dist_to[start_vertex] = 0
+    previous = [None] * graph.get_vertex_number()  # previous vertex on the path
+
+    while to_explore.size() > 0:
+        _, current_vertex = to_explore.pop()
+        for adj_vertex, edge_length in graph.adjacencies(current_vertex, True).items():
+            distance_through_current = dist_to[current_vertex] + edge_length
+            if distance_through_current < dist_to[adj_vertex]:
+                # relax a vertex
+                dist_to[adj_vertex] = distance_through_current
+                previous[adj_vertex] = current_vertex
+                if to_explore.get_key(adj_vertex) is not None:
+                    to_explore.decrease_key(distance_through_current, adj_vertex)
+                else:
+                    to_explore.push(distance_through_current, adj_vertex)
+    return get_path(target_vertex, previous), dist_to[target_vertex]
+
+
 if __name__ == '__main__':
     """
-       0---1---2
-      /\   \  /
-     /  \   \/
+    Not connected undirected graph with fixed length of edges:
+
+      0---1---2
+     /\   \  /
+    /  \   \/
     3---4---5   6
     """
     g = Graph(7)
@@ -73,7 +134,7 @@ if __name__ == '__main__':
     g.add_edge(2, 5)
 
     assert set(g.adjacencies(5)) == set([1, 2, 4])
-    assert g.adjacencies(6) == []
+    assert len(g.adjacencies(6)) == 0
 
     assert has_path_to(g, 3, 2)
     assert has_path_to(g, 0, 5)
@@ -85,5 +146,67 @@ if __name__ == '__main__':
     sp32 = get_shortest_path_to(g, 3, 2)
     assert sp32 == [3, 0, 1, 2] or sp32 == [3, 4, 5, 2]
     assert not get_shortest_path_to(g, 0, 6)
+
+    """
+    Connected undirected graph with varying length of edges:
+
+    0----1----4
+    | \ /|
+    |  2 |
+    | /  |
+    3---/
+    """
+    g = Graph(5)
+
+    g.add_edge(0, 1, 4)
+    g.add_edge(0, 3, 3)
+    g.add_edge(0, 2, 1)
+    g.add_edge(2, 1, 1)
+    g.add_edge(2, 3, 1)
+    g.add_edge(3, 1, 3)
+    g.add_edge(1, 4, 4)
+
+    assert build_mst(g) == set([(0, 2), (1, 2), (2, 3), (1, 4)])
+
+    assert get_dijkstra_shortest_path_to(g, 0, 1) == ([0, 2, 1], 2)
+    assert get_dijkstra_shortest_path_to(g, 3, 4) == ([3, 2, 1, 4], 6)
+
+    """
+    Connected undirected graph with varying length of edges:
+
+      5   5
+    0---1---2
+     \_____/
+        9
+    """
+    g = Graph(3)
+
+    g.add_edge(0, 1, 5)
+    g.add_edge(1, 2, 5)
+    g.add_edge(2, 0, 9)
+
+    assert build_mst(g) == set([(0, 1), (1, 2)])
+
+    assert get_dijkstra_shortest_path_to(g, 0, 2) == ([0, 2], 9)
+    assert get_dijkstra_shortest_path_to(g, 2, 0) == ([2, 0], 9)
+
+    """
+    Directed graph with varying length of edges:
+
+      /--->---\
+     /         \
+    0-->--1-->--2
+     \         /
+      \---<---/
+    """
+    g = Graph(3)
+
+    g.add_edge(0, 2, 10, undirected=False)
+    g.add_edge(0, 1, 4, undirected=False)
+    g.add_edge(1, 2, 4, undirected=False)
+    g.add_edge(2, 0, 10, undirected=False)
+
+    assert get_dijkstra_shortest_path_to(g, 0, 2) == ([0, 1, 2], 8)
+    assert get_dijkstra_shortest_path_to(g, 2, 0) == ([2, 0], 10)
 
     print('All tests passed')
--- heap.py
+++ heap.py
@@ -1,59 +1,126 @@
 class Heap:
-    elements = []
+    """
+    A binary heap with an ability to decrease key for a given value
+    """
+    _elements = []
+    _value2index = {}
 
-    def insert(self, element):
-        self.elements.append(element)
-        self.bubble_up(len(self.elements) - 1)
+    def push(self, key, value):
+        self._elements.append((key, value))
+        index = len(self._elements) - 1
+        self._value2index[value] = index
+        self._bubble_up(index)
 
-    def bubble_up(self, index):
-        parent = int(index / 2)  # floor - rounding down
-        parent -= 1 if index % 2 == 0 else 0
-        if parent >= 0 and self.elements[parent] > self.elements[index]:
-            self.elements[parent], self.elements[index] = self.elements[index], self.elements[parent]
-            self.bubble_up(parent)
-
-    def poll(self):
-        if not self.elements:
+    def pop(self):
+        if not self._elements:
             return None
-        head = self.elements[0]
-        last = self.elements.pop()
-        if self.elements:
-            self.elements[0] = last
-            self.bubble_down(0)
+        head = self._elements[0]
+        last = self._elements.pop()
+        if self._elements:
+            self._elements[0] = last
+            self._value2index[last[1]] = 0
+            self._bubble_down(0)
+        del self._value2index[head[1]]
         return head
 
-    def bubble_down(self, index):
+    def size(self):
+        return len(self._elements)
+
+    def decrease_key(self, key, value):
+        """
+        This method works only when all the values on the heap are unique
+        """
+        index = self._value2index[value]
+        self._elements[index] = (key, value)
+        self._bubble_up(index)
+
+    def get_key(self, value):
+        """
+        This method works only when all the values on the heap are unique
+        """
+        return None if value not in self._value2index else self._elements[self._value2index[value]][0]
+
+    def _bubble_up(self, index):
+        """
+        a.k.a. swim
+        """
+        parent = int(index / 2)  # floor - rounding down
+        parent -= 1 if index % 2 == 0 else 0
+        if parent >= 0 and self._elements[parent][0] > self._elements[index][0]:
+            parent_value = self._elements[parent][1]
+            child_value = self._elements[index][1]
+            self._elements[parent], self._elements[index] = self._elements[index], self._elements[parent]
+            self._value2index[parent_value], self._value2index[child_value] = self._value2index[child_value], self._value2index[parent_value]
+            self._bubble_up(parent)
+
+    def _bubble_down(self, index):
+        """
+        a.k.a. sink
+        """
         left = 2 * index + 1
         right = left + 1
-        if right >= len(self.elements):
-            if left >= len(self.elements):
+        if right >= len(self._elements):
+            if left >= len(self._elements):
                 return None
             else:
                 smaller_child = left
         else:
-            smaller_child = left if self.elements[left] < self.elements[right] else right
-        if self.elements[index] > self.elements[smaller_child]:
-            self.elements[index], self.elements[smaller_child] = self.elements[smaller_child], self.elements[index]
-            self.bubble_down(smaller_child)
+            smaller_child = left if self._elements[left][0] < self._elements[right][0] else right
+        if self._elements[index][0] > self._elements[smaller_child][0]:
+            parent_value = self._elements[index][1]
+            child_value = self._elements[smaller_child][1]
+            self._elements[index], self._elements[smaller_child] = self._elements[smaller_child], self._elements[index]
+            self._value2index[parent_value], self._value2index[child_value] = self._value2index[child_value], self._value2index[parent_value]
+            self._bubble_down(smaller_child)
+
 
 if __name__ == '__main__':
-    h = Heap()
+    h1 = Heap()
 
-    h.insert(3)
-    h.insert(4)
-    h.insert(2)
-    h.insert(1)
-    h.insert(5)
-    h.insert(6)
-    h.insert(0)
+    h1.push(3, 'a')
+    h1.push(4, 'b')
+    h1.push(2, 'c')
+    h1.push(1, 'd')
+    h1.push(5, 'e')
+    h1.push(6, 'f')
+    h1.push(0, 'g')
 
-    assert h.poll() == 0
-    assert h.poll() == 1
-    assert h.poll() == 2
-    assert h.poll() == 3
-    assert h.poll() == 4
-    assert h.poll() == 5
-    assert h.poll() == 6
-    assert h.poll() is None
+    assert h1.pop() == (0, 'g')
+    assert h1.pop() == (1, 'd')
+    assert h1.pop() == (2, 'c')
+    assert h1.pop() == (3, 'a')
+    assert h1.pop() == (4, 'b')
+    assert h1.pop() == (5, 'e')
+    assert h1.pop() == (6, 'f')
+    assert h1.pop() is None
+
+    h2 = Heap()
+
+    h2.push(3, 'a')
+    h2.push(4, 'b')
+    h2.push(5, 'c')
+
+    h2.decrease_key(1, 'b')
+    h2.decrease_key(0, 'a')
+    h2.decrease_key(2, 'c')
+
+    assert h2.pop() == (0, 'a')
+    assert h2.pop() == (1, 'b')
+
+    h2.push(6, 'd')
+    h2.push(7, 'e')
+    h2.push(8, 'f')
+
+    h2.decrease_key(5, 'e')
+
+    h2.push(9, 'g')
+
+    assert h2.pop() == (2, 'c')
+    assert h2.pop() == (5, 'e')
+    assert h2.pop() == (6, 'd')
+    assert h2.pop() == (8, 'f')
+    assert h2.pop() == (9, 'g')
+
+    assert h2.pop() is None
 
     print('All tests passed')
